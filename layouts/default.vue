865122fd196962af5aca0c55ab72065c2fa6b3bc
An0ncoder <an0ncoder@protonmail.com>
2020-07-05 16:45:57 +0200

    Add basic autocomplete layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,45 @@
 <template>
   <v-app dark>
-    <h1>sup</h1>
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col cols="6">
+          <v-autocomplete
+            v-model="select"
+            :items="items"
+            :search-input.sync="search"
+            append-icon="mdi-magnify"
+            clearable
+            dark
+            multiple
+            chips
+          >
+          </v-autocomplete>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      select: null,
+      search: null,
+      items: [],
+    }
+  },
+
+  watch: {
+    search(value) {
+      this.fetchSuggestions(value)
+    },
+  },
+
+  methods: {
+    fetchSuggestions(value) {
+      this.items = ['fu', 'fu so hard', 'fu wtf is this shit']
+    },
+  },
+}
 </script>
